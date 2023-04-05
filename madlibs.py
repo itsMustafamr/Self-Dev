36631f5e90e7fcb21188c96469ac83281748de59
itsMustafamr <mr.mohdmustafa007@gmail.com>
2023-04-06 01:49:31 +0530

    Madlibs_Words_Game

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+#youtuber = "Chicken fry"          #subscribe to ______#
+#print("subscribe to" + youtuber)
+#print("subscribe to {}".format(youtuber)) #put the value of youtuber  into value of curely braces
+#print(f"subscribe to {youtuber}")
+
+adj = input("Adjective:")
+verb1 = input("verb:")
+verb2 = input("verb:")
+famous_person = input("Famous macha:")
+
+madlib = f"My life be like so {adj}! It makes me so excited all the\
+ time because I love to {verb1}. Stay hyderated and {verb2} like \
+a {famous_person}!"
+
+print(madlib)
+
